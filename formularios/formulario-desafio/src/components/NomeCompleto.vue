b8ddbe88ef51158ed6b563314a6e5b4db1a437d7
WendelNunes <wendel.nunesdonizete@gmail.com>
2021-03-10 22:28:14 -0300

    Estudos de diretivas

---
+++ formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Rotulo nome="Nome">
+      <input type="text" :value="value.nome" @input="alterarNome" />
+    </Rotulo>
+    <Rotulo nome="Sobrenome">
+      <input type="text" :value="value.sobrenome" @input="alterarSobrenome" />
+    </Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from "./Rotulo.vue";
+
+export default {
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: { Rotulo },
+  methods: {
+    alterarNome(event) {
+      this.value.nome = event.target.value;
+      this.$emit("input", this.value);
+    },
+    alterarSobrenome(event) {
+      this.value.sobrenome = event.target.value;
+      this.$emit("input", this.value);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
