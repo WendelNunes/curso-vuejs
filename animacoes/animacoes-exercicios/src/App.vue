56faf779b8799cdfcdbfee1be5adff1c859c0aeb
WendelNunes <wendel.nunesdonizete@gmail.com>
2021-03-11 20:55:45 -0300

    Animações

---
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app" class="container-fluid">
+    <h1>Animações</h1>
+    <hr />
+    <b-button variant="primary" class="mb-4" @click="exibir = !exibir"
+      >Mostrar Mensagem</b-button
+    >
+
+    <transition name="fade">
+      <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Uma mensagem de informação para o usuário!",
+      exibir: false,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s;
+}
+
+/* NAO PRECISA USAR ESTAS CLASSES POR PADRAO O OPACITY E 1 */
+.fade-enter-to,
+.fade-leave {
+  opacity: 1;
+}
+</style>
