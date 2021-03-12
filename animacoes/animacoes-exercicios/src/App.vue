df2d57779012810ae6066e28f992f5e6ec7b3ff9
WendelNunes <wendel.nunesdonizete@gmail.com>
2021-03-12 13:33:47 -0300

    Transição com componente dinâmincos

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -28,20 +28,108 @@
       <option value="slide">Slide</option>
     </b-select>
 
-    <transition :name="tipoAnimacao">
-      <b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
+    <transition :name="tipoAnimacao" mode="out-in">
+      <b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
+      <b-alert variant="warning" show v-else key="warn">{{ msg }}</b-alert>
+    </transition>
+
+    <hr />
+
+    <b-button variant="primary" class="mb-4" @click="exibir2 = !exibir2"
+      >Alternar</b-button
+    >
+
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled"
+    >
+      <div class="caixa" v-if="exibir2"></div>
+    </transition>
+
+    <hr />
+
+    <div class="mb-4">
+      <b-button
+        variant="primary"
+        @click="componenteSelecionado = 'AlertaInfo'"
+        class="mr-2"
+        >Info</b-button
+      >
+      <b-button
+        variant="secundary"
+        @click="componenteSelecionado = 'AlertaAdvertencia'"
+        >Advertencia</b-button
+      >
+    </div>
+    <transition name="fade" mode="out-in">
+      <component :is="componenteSelecionado"></component>
     </transition>
   </div>
 </template>
 
 <script>
+import AlertaAdvertencia from "@/AlertaAdvertencia.vue";
+import AlertaInfo from "@/AlertaInfo.vue";
+
 export default {
+  components: { AlertaAdvertencia, AlertaInfo },
   data() {
     return {
       msg: "Uma mensagem de informação para o usuário!",
       exibir: false,
+      exibir2: true,
       tipoAnimacao: "fade",
+      larguraBase: 0,
+      componenteSelecionado: "alertaInfo",
     };
+  },
+  methods: {
+    animar(el, done, negativo) {
+      let rodada = 1;
+      const temporazidor = setInterval(() => {
+        const novaLargura =
+          this.larguraBase + (negativo ? -rodada * 10 : rodada * 10);
+        el.style.width = `${novaLargura}px`;
+        rodada++;
+        if (rodada > 30) {
+          clearInterval(temporazidor);
+          done();
+        }
+      }, 20);
+    },
+    beforeEnter(el) {
+      this.larguraBase = 0;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    enter(el, done) {
+      this.animar(el, done, false);
+    },
+    // afterEnter(el) {
+    //   console.log("afterEnter");
+    // },
+    // enterCancelled(el) {
+    //   console.log("enterCancelled");
+    // },
+    beforeLeave(el) {
+      this.larguraBase = 300;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    leave(el, done) {
+      this.animar(el, done, true);
+    },
+    // afterLeave(el) {
+    //   console.log("afterLeave");
+    // },
+    // leaveCancelled(el) {
+    //   console.log("leaveCancelled");
+    // },
   },
 };
 </script>
@@ -55,6 +143,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 1.5rem;
+}
+
+.caixa {
+  height: 100px;
+  width: 300px;
+  margin: 30px auto;
+  background-color: lightgreen;
 }
 
 .fade-enter,
