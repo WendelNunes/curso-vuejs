a143aa5bc8c3c91f8ab0bf7387edf97d690d6fe2
WendelNunes <wendel.nunesdonizete@gmail.com>
2021-03-29 14:39:07 -0300

    Implementação categoria

--- projeto-knowledge/versao-inicial/backend/config/routes.js
+++ projeto-knowledge/versao-inicial/backend/config/routes.js
@@ -4,5 +4,15 @@ module.exports = app => {
         .get(app.api.user.get);
 
     app.route("/users/:id")
-        .put(app.api.user.save);
+        .put(app.api.user.save)
+        .get(app.api.user.getById);
+
+    app.route("/categories")
+        .post(app.api.category.save)
+        .get(app.api.category.get);
+
+    app.route("/categories/:id")
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove);
 }
