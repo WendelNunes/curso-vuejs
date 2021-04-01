3d99321a25b1a3a8b5f8d4953482c2d8602e12ae
WendelNunes <wendel.nunesdonizete@gmail.com>
2021-04-01 08:07:14 -0300

    Implementando Dashboard

--- projeto-knowledge/versao-inicial/frontend/src/main.js
+++ projeto-knowledge/versao-inicial/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from "@/config/router";
 
 Vue.config.productionTip = false;
 
+// TEMPORARIO
+require("axios").defaults.headers.common["Authorization"] = "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IldlbmRlbCBOdW5lcyBEb25pemV0ZSIsImVtYWlsIjoid2VuZGVsLm51bmVzZG9uaXpldGVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYxNzI3NDUzMiwiZXhwIjoxNjE3NTMzNzMyfQ.-C5lsNqgw4Rx7Yd9eXfD33slFyan6LZh0tcb1A8S9OM";
+
 new Vue({
   store,
   router,
