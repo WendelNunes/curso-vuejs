43f33da3b650e500db26f31f291ffc6e4a7be714
WendelNunes <wendel.nunesdonizete@gmail.com>
2021-04-05 22:22:14 -0300

    Implementando login

--- projeto-knowledge/versao-inicial/frontend/src/App.vue
+++ projeto-knowledge/versao-inicial/frontend/src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app" :class="{ 'hide-menu': !isMenuVisible }">
+  <div id="app" :class="{ 'hide-menu': !isMenuVisible || !user }">
     <Header
       title="Cod3r - Base de Conhecimento"
-      :hideToggle="false"
-      :hideUserDropdown="false"
+      :hideToggle="!user"
+      :hideUserDropdown="!user"
     />
-    <Menu />
-    <Content />
+    <Menu v-if="user" />
+    <Loading v-if="validatingToken" />
+    <Content v-else />
     <Footer />
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import { baseApiUrl, userKey } from "@/global";
+import { mapState } from "vuex";
 import Header from "@/components/template/Header";
 import Menu from "@/components/template/Menu";
 import Content from "@/components/template/Content";
 import Footer from "@/components/template/Footer";
-import { mapState } from "vuex";
+import Loading from "@/components/template/Loading";
 
 export default {
   name: "App",
-  components: { Header, Menu, Content, Footer },
+  components: { Header, Menu, Content, Footer, Loading },
   computed: {
-    ...mapState(["isMenuVisible"]),
+    ...mapState(["isMenuVisible", "user"]),
+  },
+  data() {
+    return {
+      validatingToken: true,
+    };
+  },
+  methods: {
+    async validateToken() {
+      this.validatingToken = true;
+
+      const json = localStorage.getItem(userKey);
+      const userData = JSON.parse(json);
+      this.$store.commit("setUser", null);
+
+      if (!userData) {
+        this.validatingToken = false;
+        this.$router.push({ name: "auth" });
+        return;
+      }
+
+      const res = await axios.post(`${baseApiUrl}/validateToken`, userData);
+
+      if (res.data) {
+        this.$store.commit("setUser", userData);
+      } else {
+        localStorage.removeItem(userKey);
+        this.$router.push({ name: "auth" });
+      }
+
+      this.validatingToken = false;
+    },
+  },
+  created() {
+    this.validateToken();
   },
 };
 </script>
--- projeto-knowledge/versao-inicial/frontend/src/main.js
+++ projeto-knowledge/versao-inicial/frontend/src/main.js
@@ -10,9 +10,6 @@ import router from "@/config/router";
 
 Vue.config.productionTip = false;
 
-// TEMPORARIO
-require("axios").defaults.headers.common["Authorization"] = "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IldlbmRlbCBOdW5lcyBEb25pemV0ZSIsImVtYWlsIjoid2VuZGVsLm51bmVzZG9uaXpldGVAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYxNzY0MDYxMywiZXhwIjoxNjE3ODk5ODEzfQ.7GbGF42aUCko-uMzX41nfHq8Wd3iobaiiSpc_K7ZnXg";
-
 new Vue({
   store,
   router,
